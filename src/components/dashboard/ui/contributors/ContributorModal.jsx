5f0914904cb00a1d1ed7c69409d0fa1b406cc21c
Simple0x0 <simple0xx0@gmail.com>
2025-07-16 07:28:05 +0000

    New Update on Contributors API

--- src/components/dashboard/ui/contributors/ContributorModal.jsx
+++ src/components/dashboard/ui/contributors/ContributorModal.jsx
@@ -47,7 +47,6 @@ export default function ContributorModal({ isOpen, onClose, initialContributor =
     setSocialLinks(prev => prev.filter((_, i) => i !== index));
   };
 
-  
   const handleSubmit = async () => {
     const contributorData = {
       ...meta,
